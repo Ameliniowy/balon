c53db38e84be9e4e2eeb004b7a3fa33d6e3ea0d7
Ameliniowy <ameliniowy@op.pl>
2026-07-25 15:20:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,37 @@ function fly() {
 
     balloon.offsetHeight;
 
-    // szerokość balonu
+    // szerokość balonuconst balloon = document.getElementById("balloon");
+
+function fly() {
+
+    balloon.style.transition = "none";
+
+    balloon.style.bottom = "-900px";
+
+    balloon.offsetHeight;
+
+    // Trzy możliwe trasy
+    const positions = ["45%", "50%", "55%"];
+
+    // Losowanie jednej z tras
+    const randomPosition =
+        positions[Math.floor(Math.random() * positions.length)];
+
+    balloon.style.left = randomPosition;
+    balloon.style.transform = "translateX(-50%)";
+
+    // Czas lotu
+    balloon.style.transition = "bottom 10s linear";
+
+    balloon.style.bottom = "110%";
+
+}
+
+fly();
+
+// Powtarzanie co 45 sekund
+setInterval(fly, 45000);
     const balloonWidth = 350;
 
     // maksymalna pozycja X
